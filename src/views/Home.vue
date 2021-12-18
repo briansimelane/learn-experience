67c3c8782ac3978f8af28ceb679129cb7926a1c5
Brian Simelane <brian@learningsims.co.za>
2021-12-18 20:51:50 +0200

    setup for Learn Experience

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-yellow-500 ml-5 mt-5 px-2 py-2 w-24 rounded-lg">
+    This is the home page
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
